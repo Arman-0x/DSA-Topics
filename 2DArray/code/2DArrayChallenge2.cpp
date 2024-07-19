67fdd5c26ebcec458bfc1642ca3795164414d0e3
Arman <22071120@ycce.in>
2024-07-19 23:53:37 +0530

    added a example.
    day 7.

---
+++ 2DArray/code/2DArrayChallenge2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int n=3;
+	int m=3;
+	int arr[n][m];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+		
+		cin>>arr[i][j];
+		}}
+	
+	
+	for(int i=0;i<3;i++){
+		for(int j=i;j<3;j++){
+			
+		
+				int temp =arr[i][j];
+				arr[i][j]=arr[j][i];
+				arr[j][i]=temp;
+				
+				
+				
+			}
+			
+		}
+		
+		
+		for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+		cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+		}
+		
+		
+		
+		
+		
+		
+	//	cout<<endl;
+	}
+	
+	
+	
